04f536f4b47b245fd44f6f23e182f8879fc28399
Janeal Pimentel <ceejaypimentel@hotmail.ca>
2020-03-31 18:05:18 -0500

    Configures routes to only accept :json formats

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,9 @@
 class ApplicationController < ActionController::Base
+  before_action :set_default_response_format
+
+  protected
+
+  def set_default_response_format
+    request.format = :json
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -10,6 +10,6 @@ Rails.application.routes.draw do
   resources :provinces
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
-  resources :pages
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
