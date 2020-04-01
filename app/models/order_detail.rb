c8e4950fb45532e00dfc772cd8155e377ab1e54e
Janeal Pimentel <ceejaypimentel@hotmail.ca>
2020-03-31 20:58:13 -0500

    Added associations for product and orders through detail

--- app/models/order_detail.rb
+++ app/models/order_detail.rb
@@ -1,3 +1,5 @@
 class OrderDetail < ApplicationRecord
+  validates :reference_number, :total, :historical_tax, presence: :true
+  validates :historical_tax, :total, numericality: { greater_than_or_equal_to: 0.00, allow_blank: true }
   belongs_to :customer
 end
--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,7 @@
 class Product < ApplicationRecord
+  validates :name, :description, presence: :true
+  validates :price, numericality: { greater_than_or_equal_to: 0.00, allow_blank: true }
+  has_many :order_details, through: :details
   belongs_to :photographer
   belongs_to :category
 end
