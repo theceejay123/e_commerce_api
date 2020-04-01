d4bfed80206983ad397a7bee963a7ad73c630aee
Janeal Pimentel <ceejaypimentel@hotmail.ca>
2020-04-01 10:58:35 -0500

    Adds fixes to product associations

--- app/models/order_detail.rb
+++ app/models/order_detail.rb
@@ -1,7 +1,9 @@
 class OrderDetail < ApplicationRecord
   validates :reference_number, :total, :historical_tax, presence: true
   validates :historical_tax, :total, numericality: { greater_than_or_equal_to: 0.00, allow_blank: true }
-  has_many :products, through: :details
 
   belongs_to :customer
+
+  has_many :details
+  has_many :products, through: :details
 end
--- app/models/product.rb
+++ app/models/product.rb
@@ -1,10 +1,11 @@
 class Product < ApplicationRecord
   validates :name, :description, presence: true
   validates :price, numericality: { greater_than_or_equal_to: 0.00, allow_blank: true }
-  has_many :order_details, through: :details
-
   has_one_attached :prod_image
 
   belongs_to :photographer
   belongs_to :category
+
+  has_many :details
+  has_many :order_details, through: :details
 end
