06eb7c6fa9d15aff7dd192d3c4a93fdd2d2195ce
Janeal Pimentel <ceejaypimentel@hotmail.ca>
2020-04-21 17:07:32 -0500

    Creates routes for checkout

--- config/routes.rb
+++ config/routes.rb
@@ -11,6 +11,12 @@ Rails.application.routes.draw do
   get "/auto_login", to: "auth#auto_login"
   get "/user_is_authed", to: "auth#user_is_authed"
 
+  scope "checkout" do
+      post "create", to: "checkout#create", as: 'checkout_create',
+      get "success", to: "checkout#success", as: 'checkout_success',
+      get "cancel", to: "checkout#cancel", as: 'checkout_cancel',
+  end
+
   resources :taxes
   resources :provinces
   resources :pages
